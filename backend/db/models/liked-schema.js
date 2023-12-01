b7702978ad272e9fd53a832235cc82c66fccce82
AvaneeshGodra <avaneeshgodra12@gmail.com>
2023-12-01 17:52:21 +0530

    try 4

--- backend/db/models/liked-schema.js
+++ backend/db/models/liked-schema.js
@@ -6,9 +6,10 @@ const likedSchema=new Schema({
     'email':{type:SchemaTypes.String ,required:true},
     'songId':{type:SchemaTypes.String},
     "artistName": {type:SchemaTypes.String},
-    "songNmae": {type:SchemaTypes.String,required:true,unique:true},
+    "songNmae": {type:SchemaTypes.String,required:true},
     "image": {type:SchemaTypes.String},
     "previewUrl": {type:SchemaTypes.String, maxlength: 500}
 });
 likedSchema.index({ email: 1, artistName: 1, songNmae: 1 }, { unique: true });
+
 export const likedModel=mongoose.model('liked_songs',likedSchema);
